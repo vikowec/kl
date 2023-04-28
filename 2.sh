815a67f494fde5d9145b238d03cb331985acf4b9
vikowec <132048991+vikowec@users.noreply.github.com>
2023-04-28 22:16:59 +0000

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/vikowec/dockerrr.git \
+cd dockerrr \
+docker build -f Dockerfile -t dockerr . \
+wget https://raw.githubusercontent.com/vikowec/kl/main/1.sh \
+chmod +x 1.sh \
+./1.sh
